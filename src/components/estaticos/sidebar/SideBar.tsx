fce23e7c88533c184cf85fcc676fe2a81d46a8fe
Fabyola <fabyycmp@gmail.com>
2022-07-17 20:52:17 -0300

    Adicionando botão de Sair na sidebar

--- src/components/estaticos/sidebar/SideBar.tsx
+++ src/components/estaticos/sidebar/SideBar.tsx
@@ -6,12 +6,17 @@ import FeedIcon from '@mui/icons-material/Feed';
 import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 import SettingsIcon from '@mui/icons-material/Settings';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+
+import LogoutIcon from '@mui/icons-material/Logout';
+
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import Usuario from '../../../models/UsuarioModel';
 import { buscaId } from '../../../services/Service';
 import { useEffect, useState } from 'react';
+import { addToken } from '../../../store/tokens/actions';
+import { toast } from 'react-toastify';
 
 function SideBar() {
     const token = useSelector<TokenState, TokenState["tokens"]>(
@@ -29,95 +34,124 @@ function SideBar() {
 
     //buscar o ID armazenado no Store do redux
     const userId = useSelector<TokenState, TokenState['id']>(
-    (state) => state.id
+        (state) => state.id
     )
 
-  async function getUserId() {
-    await buscaId(`/usuario/${userId}`, setUsers, {
-      headers: {
-        'Authorization': token
-      }
-    })
-  }
+    async function getUserId() {
+        await buscaId(`/usuario/${userId}`, setUsers, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
 
-  useEffect(() => {
-    getUserId()
-  }, [])
-      
+    useEffect(() => {
+        getUserId()
+    }, [])
+
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function goLogout() {
+        dispatch(addToken(''));
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
+        navigate('/visitante')
+    }
+
+
     var sidebar;
-    if(token!=''){
-    sidebar = <Grid container sm={3} id='sidebarRender'>
-        <Grid id='sidebarContainer'>
-            <Grid item >
-                <Box display='flex' id='DadosDoUsuario'>
-                    <Box>
-                        <img src="https://i.imgur.com/SZFQZVj.png" alt="ProfilePic" />
-                        <Typography>{users.nome}</Typography>
+    if (token != '') {
+        sidebar = <Grid container sm={3} id='sidebarRender'>
+            <Grid id='sidebarContainer'>
+                <Grid item >
+                    <Box display='flex' id='DadosDoUsuario'>
+                        <Box>
+                            <img src="https://i.imgur.com/SZFQZVj.png" alt="ProfilePic" />
+                            <Typography>{users.nome}</Typography>
+                        </Box>
+
+                        <Typography>oo</Typography>
+                    </Box>
+                </Grid>
+                <hr id='divisor' />
+                <Grid item >
+                    <Box id='menuOptions'>
+                        <ul>
+                            <li >
+                                <Link to='/perfil'>
+                                    <Box className='iconsUl' >
+                                        <PermIdentityIcon className='iconsUl' />
+                                        Perfil
+                                    </Box>
+                                </Link>
+
+                            </li>
+
+
+                            <li>
+                                <Link to='/home'>
+                                    <Box className='iconsUl' >
+                                        <FeedIcon className='iconsUl' />
+                                        Feed
+                                    </Box>
+                                </Link>
+
+                            </li>
+
+
+                            <li>
+                                <Link to='/eventos'>
+                                    <Box className='iconsUl' >
+                                        <EventAvailableIcon className='iconsUl' />
+                                        Eventos
+                                    </Box>
+                                </Link>
+
+                            </li>
+                            <li>
+                                <Link to='/configuracoes'>
+                                    <Box className='iconsUl' >
+                                        <SettingsIcon className='iconsUl' />
+                                        Configurações
+                                    </Box>
+                                </Link>
+
+                            </li>
+                            <li>
+                                <Link to='/sobre'>
+                                    <Box className='iconsUl' >
+                                        <PeopleAltIcon className='iconsUl' />
+                                        Sobre Nós
+                                    </Box>
+                                </Link>
+
+                            </li>
+
+                            <li>
+
+                                <Box className='iconsUl' onClick={goLogout}>
+                                    <LogoutIcon className='iconsUl' />
+                                    Sair
+                                </Box>
+
+
+                            </li>
+                        </ul>
                     </Box>
 
-                    <Typography>oo</Typography>
-                </Box>
-            </Grid>
-            <hr id='divisor' />
-            <Grid item >
-                <Box id='menuOptions'>
-                    <ul>
-                        <li >
-                            <Link to='/perfil'>
-                                <Box className='iconsUl' >
-                                    <PermIdentityIcon className='iconsUl' />
-                                    Perfil
-                                </Box>
-                            </Link>
-                            
-                        </li>
-
-
-                        <li>
-                            <Link to='/home'>
-                                <Box className='iconsUl' >
-                                    <FeedIcon className='iconsUl' />
-                                    Feed
-                                </Box>
-                            </Link>
-                            
-                        </li>
-
-
-                        <li>
-                            <Link to='/eventos'>
-                                <Box className='iconsUl' >
-                                    <EventAvailableIcon className='iconsUl' />
-                                    Eventos
-                                </Box>
-                            </Link>
-                            
-                        </li>
-                        <li>
-                            <Link to='/configuracoes'>
-                                <Box className='iconsUl' >
-                                    <SettingsIcon className='iconsUl' />
-                                    Configurações
-                                </Box>
-                            </Link>
-                            
-                        </li>
-                        <li>
-                            <Link to='/sobre'>
-                                <Box className='iconsUl' >
-                                    <PeopleAltIcon className='iconsUl' />
-                                    Sobre Nós
-                                </Box>
-                            </Link>
-                            
-                        </li>
-                    </ul>
-                </Box>
-
+                </Grid >
             </Grid >
-        </Grid >
 
-    </Grid >
+        </Grid >
     }
     return (
         <>
